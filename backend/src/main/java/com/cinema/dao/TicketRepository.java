04caef0e8eb65b66a8ecb90c7fc8a7369c208678
ayoubelkass <elkass_ayoub@emsi-edu.ma>
2021-07-10 18:19:39 +0100

    new pushes

---
+++ backend/src/main/java/com/cinema/dao/TicketRepository.java
@@ -0,0 +1,30 @@
+package com.cinema.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.cinema.entities.Cinema;
+import com.cinema.entities.Salle;
+import com.cinema.entities.Ticket;
+
+@RepositoryRestResource
+@CrossOrigin("*")
+public interface TicketRepository extends JpaRepository<Ticket,Long> {
+
+}
+package com.cinema.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.cinema.entities.Cinema;
+import com.cinema.entities.Salle;
+import com.cinema.entities.Ticket;
+
+@RepositoryRestResource
+@CrossOrigin("*")
+public interface TicketRepository extends JpaRepository<Ticket,Long> {
+
+}
